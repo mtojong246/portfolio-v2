430f0e7bd7a9d5b4069a825a2fff80dcd7d5e138
Melanie Tojong <mtojong@Melanies-MacBook-Pro.local>
2023-07-31 14:33:40 -0400

    resume button sidebar

--- src/app/components/NavBar/Sidebar.tsx
+++ src/app/components/NavBar/Sidebar.tsx
@@ -15,7 +15,7 @@ export default function Sidebar({ toggleSidebar, openSidebar, handleScroll }: { 
                     <button className="text-lg text-[#CCC9EC] hover:text-[#1AFFC9] transition-all ease-in-out duration-300" value='contact' onClick={(e) => {handleScroll(e); toggleSidebar()}}>
                         <span className='text-[#1AFFC9] inline-block mr-3'>3. </span>Contact
                     </button>
-                    <a href="#_" className="relative inline-block w-full py-2 group">
+                    <a href="https://docs.google.com/document/d/1U6BWD7-W_cP7fa1Cf-9huOf2AgqC-afd570T14RBJyU/edit?usp=sharing" target='_blank' rel='noopener noreferrer' className="relative inline-block w-full py-2 group">
                         <span className="absolute inset-0 w-full h-full rounded transition duration-300 ease-out transform translate-x-1 translate-y-1 bg-[#1AFFC9] group-hover:-translate-x-0 group-hover:-translate-y-0"></span>
                         <span className="absolute inset-0 w-full h-full bg-[#001224] rounded border-2 border-[#1AFFC9] group-hover:bg-[#1AFFC9]"></span>
                         <span className="relative left-[55px] bottom-[2px] text-reg text-[#1AFFC9] group-hover:text-[#001224]">resume</span>
